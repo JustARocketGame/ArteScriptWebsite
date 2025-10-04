80146b327c3f9f18d129158fec264efb417a02e9
Artem057 <justarocketgame@gmail.com>
2025-10-04 15:19:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ def home():
 
 @app.route("/download", methods=["GET"])
 def download():
-    return redirect("https://github.com/JustARocketGame/ArteScriptDownload/releases/download/last_installer/main.exe")
+    return redirect("https://github.com/JustARocketGame/ArteScriptDownload/releases/download/last_installer/ArteScript.Installer.exe")
 
 @app.route("/logo", methods=["GET"])
 def logo():
     return send_file("as.png")
 
 if __name__ == "__main__":
+
     app.run(debug=True)
